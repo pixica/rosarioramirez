ebd2ef1cc9eb248095a37bf9708968970c197b72
pixica <e@pixica.com.mx>
2017-07-11 08:37:10 -0500

    Agregar sitio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({
+    	interval: 6000,
+        height: 700
+    });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
